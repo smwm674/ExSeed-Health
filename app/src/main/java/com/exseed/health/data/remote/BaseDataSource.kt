eb2440b153278ccb53428bf04d3a09ea9b8935dd
smwm674 <smwm674@gmail.com>
2022-04-12 03:08:32 +0100

    Remove unused code

--- app/src/main/java/com/exseed/health/data/remote/BaseDataSource.kt
+++ app/src/main/java/com/exseed/health/data/remote/BaseDataSource.kt
@@ -1,7 +1,6 @@
 package com.exseed.health.data.remote
 
 import com.exseed.health.utils.Resource
-import retrofit2.Call
 import retrofit2.Response
 
 abstract class BaseDataSource {
@@ -22,17 +21,4 @@ abstract class BaseDataSource {
     private fun <T> error(message: String): Resource<T> {
         return Resource.error("Network call has failed for a following reason: $message")
     }
-
-   /* protected fun <T> getResults(call: Call<T>): Resource<T> {
-        try {
-            val response = call.execute()
-            if (response.isSuccessful) {
-                val body = response.body()
-                if (body != null) return Resource.success(body)
-            }
-            return error(" ${response.code()} ${response.message()}")
-        } catch (e: Exception) {
-            return error(e.message ?: e.toString())
-        }
-    }*/
 }
